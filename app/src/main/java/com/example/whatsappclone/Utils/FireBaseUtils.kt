0c33121a747c80c2376d2b261edc4b665daa0187
abjeet16 <avjeetyadav00@gmail.com>
2024-05-27 23:40:08 +0530

    day3 sorry it was day 2 yesterday

--- app/src/main/java/com/example/whatsappclone/Utils/FireBaseUtils.kt
+++ app/src/main/java/com/example/whatsappclone/Utils/FireBaseUtils.kt
@@ -3,11 +3,15 @@ package com.example.whatsappclone.Utils
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 
 class FireBaseUtils {
     fun getCurrentUserId(): String? {
         return FirebaseAuth.getInstance().currentUser?.uid
+    }
+    fun getStatusReference(): CollectionReference {
+        return FirebaseFirestore.getInstance().collection("Status").document(getCurrentUserId()!!).collection("status")
     }
     fun getChatroomId(userId1: String, userId2: String): String {
         return if (userId1.hashCode() < userId2.hashCode()) {
