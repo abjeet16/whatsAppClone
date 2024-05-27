3db47314c77dc45380d4711d70e9eeed2ee6b0e8
abjeet16 <avjeetyadav00@gmail.com>
2024-05-27 23:40:19 +0530

    day3 sorry it was day 2 yesterday

---
+++ app/src/main/java/com/example/whatsappclone/datamodels/Status.kt
@@ -0,0 +1,7 @@
+package com.example.whatsappclone.datamodels
+
+data class Status(
+    var statusId: String? = null,
+    var imageUrl: String? = null,
+    var statusText: String? = null,
+    var timestamp: Long? = null)
